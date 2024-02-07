592b1c80ee58ce4f6f5e370a08453ad90944c39b
IJTINK7 <ijtink7@mail.ru>
2024-02-07 14:55:05 +0300

    Connecting "Redux Dev Tools" package

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,11 @@
 import {combineReducers, createStore} from "redux";
 import {testReducer} from "./reducers/testReducer";
 import {counterReducer} from "./reducers/counterReducer";
-
-
-// const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+import {composeWithDevTools} from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
 	test: testReducer,
 	counter: counterReducer
 })
 export type RootStateType = ReturnType<typeof rootReducer>
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, composeWithDevTools())
