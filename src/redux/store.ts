260e4bd5b8821c2beefd277dab7d853e12e7600f
IJTINK7 <ijtink7@mail.ru>
2024-02-07 17:58:26 +0300

    Adding "createSagaMiddleware" function from "redux-saga" library, sending it to "applyMiddleware" "redux" library function to "composeWithDevTools" "redux-devtools-extension" library function

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import {testReducer} from "./reducers/testReducer";
 import {counterReducer} from "./reducers/counterReducer";
 import {composeWithDevTools} from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
 
 const rootReducer = combineReducers({
 	test: testReducer,
 	counter: counterReducer
 })
 export type RootStateType = ReturnType<typeof rootReducer>
-export const store = createStore(rootReducer, composeWithDevTools())
+const sagaMiddleware = createSagaMiddleware()
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)))
